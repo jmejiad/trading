379e94dcbe11f2e9a925d977c25457955bee3608
jmejiad <33932908+jmejiad@users.noreply.github.com>
2017-11-23 10:17:44 -0500

    1
    1

--- prueba4.js
+++ prueba4.js
@@ -1,6 +1,7 @@
 var bittrex = require('./node.bittrex.api.js');
 var fs = require('fs');
 var wstream = fs.createWriteStream('Ticker_Log.csv');
+var vcont = 0;
 
 wstream.write( 'Numero, Bid, Ask, Last' );
 wstream.write('\n');
@@ -9,15 +10,15 @@ function fungetticker()
 {
 	bittrex.getticker( { market : 'BTC-ETH' }, function( data, err ) 
 	{	
-		let vbid = data.result.Bid;
-		let vbidtxt = vbid.toString();
-		let vask = data.result.Ask;
-		let vasktxt = vask.toString();
-		let vlast = data.result.Last;
-		let vlasttxt = vlast.toString();
-		let vcont = vcont + 1;
-		let vcontxt = vcont.toString();
-		let vlinetxt = vcontxt + ',' + vbidtxt + ',' + vasktxt + ',' + vlasttxt;
+		var vbid = data.result.Bid;
+		var vbidtxt = vbid.toString();
+		var vask = data.result.Ask;
+		var vasktxt = vask.toString();
+		var vlast = data.result.Last;
+		var vlasttxt = vlast.toString();
+		vcont = vcont + 1;
+		var vcontxt = vcont.toString();
+		var vlinetxt = vcontxt + ',' + vbidtxt + ',' + vasktxt + ',' + vlasttxt;
 		console.log( vlinetxt );
 		wstream.write( vlinetxt );
 		wstream.write('\n');
@@ -25,7 +26,6 @@ function fungetticker()
 					 );
 }
 
-var vcont = 0;
 setInterval(fungetticker, 20000);
 
 //wstream.end();
