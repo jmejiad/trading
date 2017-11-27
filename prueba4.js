65611c25c4ecbe86cee87e4d12c4b1b0aee75d74
jmejiad <33932908+jmejiad@users.noreply.github.com>
2017-11-27 10:46:56 -0500

    1
    1

--- prueba4.js
+++ prueba4.js
@@ -57,10 +57,8 @@ function fungetticker()
 
 function funoperacion()
 {
-	if (vestado_op = 0){ 
-	// si no hay operacion abierta
-		if (vask > vaskant) and (vaskant < vaskant2) { 
-		// si baja el precio y luego sube, y no hay operación abierta, hay que comprar 
+	if (vestado_op = 0){ // si no hay operacion abierta
+		if ((vask > vaskant) && (vaskant < vaskant2)) { // si baja el precio y luego sube, y no hay operación abierta, hay que comprar 
 			vestado_op = 1;
 			vaccion = 'Comprar';
 			//funcompra();
